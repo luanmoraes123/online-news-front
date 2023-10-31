a2b5411f4d5b64d384e7a76e815385d389cfeefa
luan moraes <luan.moraees21@gmail.com>
2023-10-31 10:31:20 -0300

    criando o menu

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,6 +1,34 @@
+'use client'
+import './style.css'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation' 
+
 const Menu = () =>{
+  const [menuLateral, setMenuLateral] = useState(false);
+  const router = useRouter();
   return (
-    <nav>Menu</nav>
+    <>
+    <nav className='menu'>
+      <button onClick={() => setMenuLateral(!menuLateral)}>Menu</button>
+      <div>Logo</div>
+      <button onClick={() => router.push('./login')}>Login</button>
+    </nav>
+    {
+      menuLateral &&
+      <div className="menu_lateral">
+        <div className='fechar'>
+          <button onClick={() => setMenuLateral(!menuLateral)}>x</button>
+        </div>
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      </div>
+      
+    }
+    
+    </>
   );
 }
 
